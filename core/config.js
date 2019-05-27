e58aadb938964b1e94841887d0d7fb945d5f0c8c
Daniel <d.feliz@gbh.com.do>
2019-05-27 15:59:28 -0400

    Send email to user successfully

--- core/config.js
+++ core/config.js
@@ -5,5 +5,8 @@ const dialect = "mysql";
 const host = "localhost";
 const port = 3000;
 
+const emailUsername = "dfpokedex@gmail.com"
+const emailPassword = "1887415157l"
 
-module.exports = {database, user, password, host, dialect, port};
+
+module.exports = {database, user, password, host, dialect, port, emailUsername, emailPassword};
