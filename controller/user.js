2acfe3d7c9e4a49896821370637909e78e2d417e
Daniel <d.feliz@gbh.com.do>
2019-05-28 11:52:13 -0400

    Add JWT generator and verificator

--- controller/user.js
+++ controller/user.js
@@ -1,36 +1,45 @@
 const validator = require('../helpers/validators/user');
 const userServices = require('../services/user');
-const httpMsgs = require('../helpers/httpMsgs');
+const httpMsgs = require('../helpers/httpMsgs/httpMsgs');
 
-exports.register = async (req, res, next) => {
+exports.register = async (req, res) => {
+    let isValid = validator.validateUser(req.body.user);
     const userExists = await validator.checkUserExists(req.body.user);
-    if (userExists === true) {
-        let err = "Username already exists in database";
-        httpMsgs.show409(req, res, err);
-    } 
-    else {
-        const emailExists = await validator.checkEmailExists(req.body.user);
-        if (emailExists === true) {
-            let err = "Email already exists in database";
+    
+    if (isValid) {
+        if (userExists === true) {
+            let err = "Username already exists in database";
             httpMsgs.show409(req, res, err);
         } 
-        if (userExists === false && emailExists === false) {
-            console.log("Usuario no existe y email no existe, registrando usuario")
-            userServices.registerUser(req.body.user, res);
-        }
+        else {
+            const emailExists = await validator.checkEmailExists(req.body.user);
+            if (emailExists === true) {
+                let err = "Email already exists in database";
+                httpMsgs.show409(req, res, err);
+            } 
+            if (userExists === false && emailExists === false) {
+                console.log("Usuario no existe y email no existe, registrando usuario")
+                userServices.registerUser(req.body.user, res);
+            }
+        }    
+    }
+    else {
+        let err = "Missing data";
+        httpMsgs.show400(req, res, err);
     }
     
 } 
 
-exports.activate = (req, res, next) => {
+exports.activate = (req, res) => {
     const {email, hash} = req.body;
     userServices.activateUser(email, hash, res)
 }
 
-exports.login = (req, res, next) => {
-
+exports.login = (req, res) => {
+    const {username, password} = req.body.user;
+    userServices.logIn(username, password, res)
 }
 
-exports.getUsers = (req, res, next) => {
+exports.getUsers = (req, res) => {
     userServices.getUsers(req, res);
 }
--- helpers/validators/user.js
+++ helpers/validators/user.js
@@ -1,5 +1,12 @@
 const User = require('../../models/user');
 
+exports.validateUser = (data) => {
+    if (data.user_name && data.user_lastname && data.user_birthdate && data.user_city && data.user_email && data.user_username && data.user_password) {
+        return true;
+    }
+    return false;
+}
+
 exports.checkUserExists = async (data) => {
     let count;
     await User.count({ where: {user_username: data.user_username }})
@@ -9,10 +16,8 @@ exports.checkUserExists = async (data) => {
             console.log(err);
         })
     if (count >= 1) {
-        console.log("Existe, no insertar")
         return true;
     }
-    console.log("No existe, insertar")
     return false;
     }
 
